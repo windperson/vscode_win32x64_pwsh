f5e84aedacddeb1fd5525e761247fa41f1c48710
Isak Pao <windperson@gmail.com>
2024-11-29 17:33:53 +0800

    Add dotnet sdk autocomplete

--- pwsh-scripts/Set-PwshTools.ps1
+++ pwsh-scripts/Set-PwshTools.ps1
@@ -1,12 +1,22 @@
 # Shows navigable menu of all options when hitting Tab
 Set-PSReadLineKeyHandler -Key Tab -Function MenuComplete
 
-# Autocompleteion for Arrow keys
+# Auto complete for Arrow keys
 Set-PSReadLineOption -HistorySearchCursorMovesToEnd
 Set-PSReadLineKeyHandler -Key UpArrow -Function HistorySearchBackward
 Set-PSReadLineKeyHandler -Key DownArrow -Function HistorySearchForward
 
 Set-PSReadLineOption -ShowToolTips
+
+# dotnet SDK tab completion https://learn.microsoft.com/dotnet/core/tools/enable-tab-autocomplete#powershell
+if (Get-Command dotnet -ErrorAction Ignore) {
+    Register-ArgumentCompleter -Native -CommandName dotnet -ScriptBlock {
+        param($wordToComplete, $commandAst, $cursorPosition)
+        dotnet complete --position $cursorPosition "$commandAst" | ForEach-Object {
+            [System.Management.Automation.CompletionResult]::new($_, $_, 'ParameterValue', $_)
+        }
+    }
+}
 
 function Touch ($file) {
     if (Test-Path $file) {
